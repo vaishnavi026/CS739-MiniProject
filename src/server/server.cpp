2c8f2ad242846d560b62b4965ec75adaaea9fcc2
Aditya Kolsur <kolsur@wisc.edu>
2024-09-23 22:41:47 -0500

    Refactor & client.h

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,44 +1,44 @@
+#include "KeyValueController.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
 #include <iostream>
 #include <string>
-#include <grpcpp/grpcpp.h>
-#include "KeyValueController.grpc.pb.h"
-
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-using KVStore::kvStore;
-using KVStore::PutRequest;
-using KVStore::PutResponse;
-using KVStore::GetRequest;
-using KVStore::GetReponse;
+using kvstore::GetReponse;
+using kvstore::GetRequest;
+using kvstore::KVStore;
+using kvstore::PutRequest;
+using kvstore::PutResponse;
 
-class KVStoreServiceImpl final : public kvStore::Service {
+class KVStoreServiceImpl final : public KVStore::Service {
 public:
-    Status Put(ServerContext* context, const PutRequest* request, PutResponse* response) override {
-        return Status::OK;
-    }
+  Status Put(ServerContext *context, const PutRequest *request,
+             PutResponse *response) override {
+    return Status::OK;
+  }
 
-    Status Get(ServerContext* context, const GetRequest* request, GetReponse* response) override {
-        return Status::OK;
-    }
-
+  Status Get(ServerContext *context, const GetRequest *request,
+             GetReponse *response) override {
+    return Status::OK;
+  }
 };
 
 void RunServer() {
-    std::string server_address("0.0.0.0:50051");
-    KVStoreServiceImpl service;
+  std::string server_address("0.0.0.0:50051");
+  KVStoreServiceImpl service;
 
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    std::unique_ptr<Server> server(builder.BuildAndStart());
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
 
-    server->Wait();
+  server->Wait();
 }
 
-int main(int argc, char** argv) {
-    RunServer();
-    return 0;
+int main(int argc, char **argv) {
+  RunServer();
+  return 0;
 }
