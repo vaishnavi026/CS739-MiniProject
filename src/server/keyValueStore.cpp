ba37bd8fa13da9d8a57cc71510340800b5531676
Aditya Kolsur <kolsur@wisc.edu>
2024-09-30 10:05:01 -0500

    Sharding fixed

--- src/server/keyValueStore.cpp
+++ src/server/keyValueStore.cpp
@@ -7,304 +7,183 @@
 #include <string.h>
 
 keyValueStore::keyValueStore() {
-  // Open the SQLite database and establish the connection
-  // sqlite3_config(SQLITE_CONFIG_SERIALIZED);
-  sqlite3* db0 = nullptr;
-  sqlite3* db1 = nullptr;
-  sqlite3* db2 = nullptr;
-  sqlite3* db3 = nullptr;
-  sqlite3* db4 = nullptr;
-  sqlite3* db5 = nullptr;
-  sqlite3* db6 = nullptr;
-  sqlite3* db7 = nullptr;
-  
-  opendb("kv0.db",db0);
-  opendb("kv1.db",db1);
-  opendb("kv2.db",db2);
-  opendb("kv3.db",db3);
-  opendb("kv4.db",db4);
-  opendb("kv5.db",db5);
-  opendb("kv6.db",db6);
-  opendb("kv7.db",db7);
-  
-
-  // int rc = sqlite3_open("kv1.db", &db1);
-
-  // if (rc) {
-  //   fprintf(stderr, "Can't open database kv1: %s\n", sqlite3_errmsg(db1));
-  //   exit(1);
-  // }
-
-  // Create a kv table in the database
-  tablename = "kv_store";
-  const char *sql =
-      "CREATE TABLE IF NOT EXISTS kv_store (key TEXT PRIMARY KEY, value TEXT);";
-  // char *err_msg = nullptr;
-
-  // rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-  // if (rc != SQLITE_OK) {
-  //   fprintf(stderr, "SQL error: %s\n", err_msg);
-  //   sqlite3_free(err_msg);
-  //   exit(1);
-  // }
-
-  execdb(db0,sql);
-  execdb(db1,sql);
-  execdb(db2,sql);
-  execdb(db3,sql);
-  execdb(db4,sql);
-  execdb(db5,sql);
-  execdb(db6,sql);
-  execdb(db7,sql);
-  
-
-  // reader_count = 0;
+  for (int i = 0; i < 8; i++) {
+    opendb("kv" + std::to_string(i) + ".db", &shards[i]);
+    execdb(&shards[i], "CREATE TABLE IF NOT EXISTS kv_store (key TEXT PRIMARY "
+                       "KEY, value TEXT);");
+  }
 }
 
 keyValueStore::~keyValueStore() {
   // Close the connection to the database
-  sqlite3_close(db0);
-  sqlite3_close(db1);
-  sqlite3_close(db2);
-  sqlite3_close(db3);
-  sqlite3_close(db4);
-  sqlite3_close(db5);
-  sqlite3_close(db6);
-  sqlite3_close(db7);
-  
+  for (int i = 0; i < 8; i++) {
+    sqlite3_close(shards[i]);
+  }
 }
 
-void keyValueStore::opendb(const std::string &db_name, sqlite3* &db){
-    int rc = sqlite3_open(db_name.c_str(), &db);
+void keyValueStore::opendb(const std::string &db_name, sqlite3 **db) {
+  int rc = sqlite3_open(db_name.c_str(), &*db);
 
-    if (rc) {
-        fprintf(stderr, "Can't open database %s: %s\n", db_name.c_str(), sqlite3_errmsg(db));
-        exit(1);
-    }
+  if (rc) {
+    fprintf(stderr, "Can't open database %s: %s\n", db_name.c_str(),
+            sqlite3_errmsg(*db));
+    exit(1);
+  }
 }
 
-void keyValueStore::execdb(sqlite3* db, const char* sql){
-    
-    char *err_msg = nullptr;
-    int rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-        exit(1);
-    }
+void keyValueStore::execdb(sqlite3 **db, const char *sql) {
+  char *err_msg = nullptr;
+  int rc = sqlite3_exec(*db, sql, 0, 0, &err_msg);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", err_msg);
+    sqlite3_free(err_msg);
+    exit(1);
+  }
 }
 
-int keyValueStore::readdb(sqlite3* db,char *key, std::string &value){
-    
-    int rc;
-    const char *read_query = "SELECT value FROM kv_store WHERE KEY = ?;";
-    sqlite3_stmt *Stmt = nullptr;
+int keyValueStore::readdb(sqlite3 *db, const char *key, std::string &value) {
 
-    int sqlite_rc = sqlite3_prepare_v2(db, read_query, -1, &Stmt, nullptr);
+  int rc = -1;
+  const char *read_query = "SELECT value FROM kv_store WHERE KEY = ?;";
+  sqlite3_stmt *Stmt = nullptr;
 
-    if (sqlite_rc != SQLITE_OK) {
-      fprintf(stderr, "Read query : SQLite prepare error : %s\n",
-      sqlite3_errmsg(db));
-      rc = -1;
-    }
+  int sqlite_rc = sqlite3_prepare_v2(db, read_query, -1, &Stmt, nullptr);
 
-    sqlite3_bind_text(Stmt, 1, key, -1, SQLITE_TRANSIENT);
-    sqlite_rc = sqlite3_step(Stmt);
+  if (sqlite_rc != SQLITE_OK) {
+    fprintf(stderr, "Read query : SQLite prepare error : %s\n",
+            sqlite3_errmsg(db));
+    rc = -1;
+  }
 
-    if (sqlite_rc == SQLITE_ROW) {
-      char *column = (char *)sqlite3_column_text(Stmt, 0);
-      if (column) {
-        value = column;
-        rc = 0;
-      } else {
-        rc = 1;
-      }
-    } else if (sqlite_rc == SQLITE_DONE) {
+  sqlite3_bind_text(Stmt, 1, key, -1, SQLITE_TRANSIENT);
+  sqlite_rc = sqlite3_step(Stmt);
+
+  if (sqlite_rc == SQLITE_ROW) {
+    char *column = (char *)sqlite3_column_text(Stmt, 0);
+    if (column) {
+      value = column;
+      rc = 0;
+    } else {
       rc = 1;
-    } else {
-      fprintf(stderr, "Read Query : Failed to read from the database : %s\n",
-      sqlite3_errmsg(db));
-      rc = -1;
     }
+  } else if (sqlite_rc == SQLITE_DONE) {
+    rc = 1;
+  } else {
+    fprintf(stderr, "Read Query : Failed to read from the database : %s\n",
+            sqlite3_errmsg(db));
+    rc = -1;
+  }
 
-    sqlite3_finalize(Stmt);
+  sqlite3_finalize(Stmt);
 
-    return rc;
+  return rc;
 }
 
-int keyValueStore::writedb(sqlite3* db,char *key, char *value, std::string &old_value){
-    
-    int rc;
-    const char *read_query = "SELECT value FROM kv_store WHERE KEY = ?;";
-    const char *write_query;
-    const char *read_value;
+int keyValueStore::writedb(sqlite3 *db, const char *key, const char *value,
+                           std::string &old_value) {
 
-    sqlite3_stmt *read_Stmt = nullptr;
-    sqlite3_stmt *write_Stmt = nullptr;
+  int rc = -1;
+  const char *read_query = "SELECT value FROM kv_store WHERE KEY = ?;";
+  const char *write_query;
+  const char *read_value = "";
 
-    int sqlite_rc = sqlite3_prepare_v2(db, read_query, -1, &read_Stmt, nullptr);
+  sqlite3_stmt *read_Stmt = nullptr;
+  sqlite3_stmt *write_Stmt = nullptr;
 
+  int sqlite_rc = sqlite3_prepare_v2(db, read_query, -1, &read_Stmt, nullptr);
+
+  if (sqlite_rc != SQLITE_OK) {
+    fprintf(stderr, "Read in Write query prepare error : %s\n",
+            sqlite3_errmsg(db));
+    rc = -1;
+  }
+
+  sqlite3_bind_text(read_Stmt, 1, key, -1, SQLITE_TRANSIENT);
+  sqlite_rc = sqlite3_step(read_Stmt);
+
+  if (sqlite_rc == SQLITE_ROW) {
+    read_value = (const char *)sqlite3_column_text(read_Stmt, 0);
+    if (read_value)
+      rc = 0;
+    else
+      rc = 1;
+  } else if (sqlite_rc == SQLITE_DONE) {
+    rc = 1;
+  } else {
+    fprintf(stderr,
+            "Read in Write Query : Failed to read from the database : %s\n",
+            sqlite3_errmsg(db));
+    rc = -1;
+  }
+
+  // Deciding whether to insert the key or update the key
+  if (rc == 1) {
+    write_query = "INSERT INTO kv_store (value, key) VALUES (?, ?);";
+  } else {
+    write_query = "UPDATE kv_store SET value = ? WHERE key = ?;";
+  }
+
+  if (rc == -1) {
+    // Not writing if the read has failed
+  } else if (rc == 0 && strcmp(value, read_value) == 0) {
+    old_value = read_value;
+    // Skipping write because read value same as the value to be written
+  } else {
+    old_value = read_value;
+    sqlite_rc = sqlite3_prepare_v2(db, write_query, -1, &write_Stmt, nullptr);
     if (sqlite_rc != SQLITE_OK) {
-        fprintf(stderr, "Read in Write query prepare error : %s\n",
-        sqlite3_errmsg(db));
-        rc = -1;
+      fprintf(stderr, "Write in Write query prepare error : %s\n",
+              sqlite3_errmsg(db));
     }
 
-    sqlite3_bind_text(read_Stmt, 1, key, -1, SQLITE_TRANSIENT);
-    sqlite_rc = sqlite3_step(read_Stmt);
+    sqlite3_bind_text(write_Stmt, 1, value, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(write_Stmt, 2, key, -1, SQLITE_TRANSIENT);
 
-    if (sqlite_rc == SQLITE_ROW) {
-        read_value = (const char *)sqlite3_column_text(read_Stmt, 0);
-        if (read_value)
-            rc = 0;
-        else
-            rc = 1;
-    } else if (sqlite_rc == SQLITE_DONE) {
-        rc = 1;
-    } else {
-        fprintf(stderr,
-                "Read in Write Query : Failed to read from the database : %s\n",
-                sqlite3_errmsg(db));
-        rc = -1;
+    sqlite_rc = sqlite3_step(write_Stmt);
+
+    if (sqlite_rc != SQLITE_DONE) {
+      fprintf(stderr,
+              "Write in Write Query : Failed to read from the database : %s\n",
+              sqlite3_errmsg(db));
     }
+    sqlite3_finalize(write_Stmt);
+  }
 
-    // Deciding whether to insert the key or update the key
-    if (rc == 1) {
-        write_query = "INSERT INTO kv_store (value, key) VALUES (?, ?);";
-    } else {
-        write_query = "UPDATE kv_store SET value = ? WHERE key = ?;";
-    }
+  sqlite3_finalize(read_Stmt);
 
-    if (rc == -1) {
-        // Not writing if the read has failed
-    } else if (rc == 0 && strcmp(value, read_value) == 0) {
-        old_value = read_value;
-        // Skipping write because read value same as the value to be written
-    } else {
-        old_value = read_value;
-        sqlite_rc = sqlite3_prepare_v2(db, write_query, -1, &write_Stmt, nullptr);
-        if (sqlite_rc != SQLITE_OK) {
-            fprintf(stderr, "Write in Write query prepare error : %s\n",sqlite3_errmsg(db));
-        }
-
-        sqlite3_bind_text(write_Stmt, 1, value, -1, SQLITE_TRANSIENT);
-        sqlite3_bind_text(write_Stmt, 2, key, -1, SQLITE_TRANSIENT);
-
-        sqlite_rc = sqlite3_step(write_Stmt);
-
-        if (sqlite_rc != SQLITE_DONE) {
-            fprintf(stderr,"Write in Write Query : Failed to read from the database : %s\n",sqlite3_errmsg(db));
-        }
-        sqlite3_finalize(write_Stmt);
-    }
-
-    sqlite3_finalize(read_Stmt);
-
-    return rc;
-
+  return rc;
 }
 
-int keyValueStore::read(char *key, std::string &value) {
+int keyValueStore::read(const std::string &key, std::string &value) {
 
-    size_t hash_value = hash_fn(key);
-    int db_idx = (int)hash_value % 8;
-    std::cout << "Reading from database number " << db_idx << "\n";
-    int rc;
-    
-    switch(db_idx){
-        case 0:
-            db_mutexes[0].lock();
-            rc = readdb(db0,key,value);
-            db_mutexes[0].unlock();
-            break;
-        case 1:
-            db_mutexes[1].lock();
-            rc = readdb(db1,key,value);
-            db_mutexes[1].unlock();
-            break;
-        case 2:
-            db_mutexes[2].lock();
-            rc = readdb(db2,key,value);
-            db_mutexes[2].unlock();
-            break;
-        case 3: 
-            db_mutexes[3].lock();
-            rc = readdb(db3,key,value);
-            db_mutexes[3].unlock();
-            break;
-        case 4:
-            db_mutexes[4].lock();
-            rc = readdb(db4,key,value);
-            db_mutexes[4].unlock();
-            break;
-        case 5:
-            db_mutexes[5].lock();
-            rc = readdb(db5,key,value);
-            db_mutexes[5].unlock();
-            break;
-        case 6:
-            db_mutexes[6].lock();
-            rc = readdb(db6,key,value);
-            db_mutexes[6].unlock();
-            break;
-        case 7:
-            db_mutexes[7].lock();
-            rc = readdb(db7,key,value);
-            db_mutexes[7].unlock();
-            break;
-    }
+  size_t hash_value = hash_fn(key);
+  int db_idx = hash_value % 8;
+  std::cout << "Reading from database number " << db_idx << "\n";
+  int rc;
 
-    return rc;
-
+  if (db_idx >= 0 && db_idx < 8) {
+    db_mutexes[db_idx].lock();
+    rc = readdb(shards[db_idx], key.c_str(), value);
+    db_mutexes[db_idx].unlock();
+  } else {
+    std::cerr << "Hashed DB Index Invalid" << std::endl;
+  }
+  return rc;
 }
 
-int keyValueStore::write(char *key, char *value, std::string &old_value) {
-    
-    size_t hash_value = hash_fn(key);
-    int db_idx = (int)hash_value % 8;
-    std::cout << "Writing to database number " << db_idx << "\n";
-    int rc;
-    
-    switch(db_idx){
-        case 0:
-            db_mutexes[0].lock();
-            rc = writedb(db0,key,value,old_value);
-            db_mutexes[0].unlock();
-            break;
-        case 1:
-            db_mutexes[1].lock();
-            rc = writedb(db1,key,value,old_value);
-            db_mutexes[1].unlock();
-            break;
-        case 2:
-            db_mutexes[2].lock();
-            rc = writedb(db2,key,value,old_value);
-            db_mutexes[2].unlock();
-            break;
-        case 3: 
-            db_mutexes[3].lock();
-            rc = writedb(db3,key,value,old_value);
-            db_mutexes[3].unlock();
-            break;
-        case 4:
-            db_mutexes[4].lock();
-            rc = writedb(db4,key,value,old_value);
-            db_mutexes[4].unlock();
-            break;
-        case 5:
-            db_mutexes[5].lock();
-            rc = writedb(db5,key,value,old_value);
-            db_mutexes[5].unlock();
-            break;
-        case 6:
-            db_mutexes[6].lock();
-            rc = writedb(db6,key,value,old_value);
-            db_mutexes[6].unlock();
-            break;
-        case 7:
-            db_mutexes[7].lock();
-            rc = writedb(db7,key,value,old_value);
-            db_mutexes[7].unlock();
-            break;
-    }
+int keyValueStore::write(const std::string &key, const std::string &value,
+                         std::string &old_value) {
+
+  size_t hash_value = hash_fn(key);
+  int db_idx = hash_value % 8;
+  std::cout << "Writing to database number " << db_idx << "\n";
+  int rc;
+  if (db_idx >= 0 && db_idx < 8) {
+    db_mutexes[db_idx].lock();
+    rc = writedb(shards[db_idx], key.c_str(), value.c_str(), old_value);
+    db_mutexes[db_idx].unlock();
+  } else {
+    std::cerr << "Hashed DB Index Invalid" << std::endl;
+  }
+  return rc;
 }
--- src/server/keyValueStore.h
+++ src/server/keyValueStore.h
@@ -5,33 +5,27 @@
 #include <sqlite3.h>
 class keyValueStore {
 private:
-  sqlite3 *db0;
-  sqlite3 *db1;   
-  sqlite3 *db2; 
-  sqlite3 *db3; 
-  sqlite3 *db4; 
-  sqlite3 *db5; 
-  sqlite3 *db6; 
-  sqlite3 *db7;  
-  char *tablename; // Name of the table in DB (Storing key values persistently)
-  //std::mutex resource_mutex;
+  sqlite3 *shards[8];
   std::mutex db_mutexes[8];
-  std::hash<char*> hash_fn;
-  //std::mutex read_count_mutex;
-  //int reader_count = 0;
+  std::hash<std::string> hash_fn;
+  // std::mutex read_count_mutex;
+  // int reader_count = 0;
 
-  int readdb(sqlite3 *db, char *key, std::string &value);
-  int writedb(sqlite3 *db, char *key, char *value, std::string &old_value);
-  void opendb(const std::string &db_name, sqlite3* &db);
-  void execdb(sqlite3* db, const char* sql);
+  int readdb(sqlite3 *db, const char *key, std::string &value);
+  int writedb(sqlite3 *db, const char *key, const char *value,
+              std::string &old_value);
+  void opendb(const std::string &db_name, sqlite3 **db);
+  void execdb(sqlite3 **db, const char *sql);
+
 public:
   // Constructor and Destructor
   keyValueStore();
   ~keyValueStore();
 
   // Member functions to get and put key-value pairs
-  int read(char *key, std::string &value);
-  int write(char *key, char *value, std::string &old_value);
+  int read(const std::string &key, std::string &value);
+  int write(const std::string &key, const std::string &value,
+            std::string &old_value);
 };
 
 #endif // KEYVALUESTORE_H
