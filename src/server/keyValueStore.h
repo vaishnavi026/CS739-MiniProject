01d359dd3d85b0d79720e2d58984cd2ba6c262e7
Manu Maheshwari <manum@Manus-Air.lan>
2024-09-29 23:56:36 -0500

    Pushing code for sharded db, based on key, one compile issue coming

--- src/server/keyValueStore.h
+++ src/server/keyValueStore.h
@@ -5,12 +5,25 @@
 #include <sqlite3.h>
 class keyValueStore {
 private:
-  sqlite3 *db;     // Database handle
+  sqlite3 *db0;
+  sqlite3 *db1;   
+  sqlite3 *db2; 
+  sqlite3 *db3; 
+  sqlite3 *db4; 
+  sqlite3 *db5; 
+  sqlite3 *db6; 
+  sqlite3 *db7;  
   char *tablename; // Name of the table in DB (Storing key values persistently)
-  std::mutex resource_mutex;
-  std::mutex read_count_mutex;
-  int reader_count = 0;
+  //std::mutex resource_mutex;
+  std::mutex db_mutexes[8];
+  std::hash<char*> hash_fn;
+  //std::mutex read_count_mutex;
+  //int reader_count = 0;
 
+  int readdb(sqlite3 *db, char *key, std::string &value);
+  int writedb(sqlite3 *db, char *key, char *value, std::string &old_value);
+  void opendb(const std::string &db_name, sqlite3 *db);
+  void execdb(sqlite3* db, const char* sql);
 public:
   // Constructor and Destructor
   keyValueStore();
